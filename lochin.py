e231f8a13d48728cbe973f6846603165f136f437
rashidDev2001 <168219700+rashidDev2001@users.noreply.github.com>
2024-11-11 20:18:36 +0500

    asd

--- lochin.py
+++ lochin.py
@@ -4,50 +4,45 @@ import time
 import os
 import subprocess
 
-
-
-
 try:
     check_pip3 = subprocess.check_output('dpkg -s python3-pip', shell=True)
-    if str('install ok installed') in str(check_pip3):
+    if 'install ok installed' in str(check_pip3):
         pass
 except subprocess.CalledProcessError:
     print('[+] pip3 not installed')
-    subprocess.check_output('sudo apt update',shell=True)
+    subprocess.check_output('sudo apt update', shell=True)
     subprocess.check_output('sudo apt install python3-pip -y', shell=True)
     print("[!] pip3 muvaffaqiyatli o'rnatildi")
 
-
-
 try:
-
     import requests
-except Exception:
+except ImportError:
     print("[+] python3 so'rovlari o'rnatilmagan")
     os.system('pip3 install requests')
     os.system('pip3 install requests[socks]')
     print('[!] python3 requests is installed ')
-try:
 
+try:
     check_tor = subprocess.check_output('which tor', shell=True)
 except subprocess.CalledProcessError:
-
-    print('[+] tor is not installed !')
-    subprocess.check_output('sudo apt update',shell=True)
-    subprocess.check_output('sudo apt install tor -y',shell=True)
-    print("[!]tor muvaffaqiyatli o'rnatildi" )
+    print('[+] tor is not installed!')
+    subprocess.check_output('sudo apt update', shell=True)
+    subprocess.check_output('sudo apt install tor -y', shell=True)
+    print("[!] tor muvaffaqiyatli o'rnatildi")
 
 os.system("clear")
+
 def ma_ip():
-    url='https://www.myexternalip.com/raw'
-    get_ip= requests.get(url,proxies=dict(http='socks5://127.0.0.1:9050',https='socks5://127.0.0.1:9050'))
+    url = 'https://www.myexternalip.com/raw'
+    proxies = {'http': 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'}
+    get_ip = requests.get(url, proxies=proxies)
     return get_ip.text
 
 def change():
     os.system("service tor reload")
-    print ('[+] Your IP has been Changed to : '+str(ma_ip()))
+    print('[+] Your IP has been changed to: ' + str(ma_ip()))
 
-print('''\033[1;32;40m \n
+print(r''' 
                         
  _               _     _         _         _     _ 
 | |    ___   ___| |__ (_)_ __   | | _____ ( )___(_)
@@ -58,35 +53,36 @@ print('''\033[1;32;40m \n
  Tempiltin                                         
                                                    
             https://tempiltin.uz/                   
-Dastur o'quv mashg'ulotlari maqsadida ishlab chiqilgan! \n
-        ILTIMOS YOMON MAQSADDA FOYDALANMANG ! \n
-      
+Dastur o'quv mashg'ulotlari maqsadida ishlab chiqilgan! 
+        ILTIMOS YOMON MAQSADDA FOYDALANMANG!
       
 ''')
 print("\033[1;40;31m https://www.tempiltin.uz /\n")
 
-os.system("Lochin ko'zi dasturi ishga tushdi!")
-
-
-
+print("Lochin ko'zi dasturi ishga tushdi!")
 
 time.sleep(3)
-print("\033[1;32;40m change your  SOCKES to 127.0.0.1:9050 \n")
-os.system("Lochin ko'zi dasturini boshlash")
+print("\033[1;32;40m change your SOCKS to 127.0.0.1:9050 \n")
+
 x = input("[+] Sec ichida IP o'zgartirish vaqti [type=60] >> ")
-lin = input("[+] cheksiz IP oʻzgartirish turiga [type=1000] IP-ni necha marta oʻzgartirmoqchisiz [0] >>")
-if int(lin) ==int(0):
+lin = input("[+] cheksiz IP oʻzgartirish turiga [type=1000] IP-ni necha marta oʻzgartirmoqchisiz [0] >> ")
 
-	while True:
-		try:
-			time.sleep(int(x))
-			change()
-		except KeyboardInterrupt:
+try:
+    x = int(x)
+    lin = int(lin)
+except ValueError:
+    print("Iltimos, butun son kiriting.")
+    quit()
 
-		 	print("\nLochin ko'zi is closed ")
-		 	quit()
-
+if lin == 0:
+    while True:
+        try:
+            time.sleep(x)
+            change()
+        except KeyboardInterrupt:
+            print("\nLochin ko'zi is closed")
+            quit()
 else:
-	for i in range(int(lin)):
-		    time.sleep(int(x))
-		    change()
+    for i in range(lin):
+        time.sleep(x)
+        change()
